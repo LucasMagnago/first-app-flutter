3198904eb7c74d93af066fa2212f315e0c9b5bf0
Lucas Magnago de Oliveira <lucas.magnago.02@gmail.com>
2023-09-30 17:04:44 -0300

    ajustando sobreposição do teclado e impedindo scroll da página

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -13,149 +13,159 @@ class _LoginPageState extends State<LoginPage> {
     return SafeArea(
       child: Scaffold(
         backgroundColor: const Color.fromRGBO(30, 25, 44, 1),
-        body: Center(
-          child: Column(
-            // mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              const SizedBox(
-                height: 50,
-              ),
-              Image.network(
-                'https://lp.dio.me/wp-content/uploads/2023/03/LOGO-DIO-COLOR.png',
-                height: 90,
-              ),
-              const SizedBox(
-                height: 35,
-              ),
-              const Text(
-                'Já tem cadastro?',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 28,
+        body: SingleChildScrollView(
+          physics: const ScrollPhysics(
+            parent: NeverScrollableScrollPhysics(),
+          ),
+          child: ConstrainedBox(
+            constraints: BoxConstraints(
+              maxHeight: MediaQuery.of(context).size.height,
+            ),
+            child: Column(
+              // mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const SizedBox(
+                  height: 50,
                 ),
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              const Text(
-                'Faça seu login e make the change._',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 15,
+                Image.network(
+                  'https://lp.dio.me/wp-content/uploads/2023/03/LOGO-DIO-COLOR.png',
+                  height: 90,
                 ),
-              ),
-              const SizedBox(
-                height: 35,
-              ),
-              Container(
-                width: double.infinity,
-                height: 50,
-                margin: const EdgeInsets.all(15),
-                alignment: Alignment.centerLeft,
-                child: const TextField(
-                  decoration: InputDecoration(
-                    enabledBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(
-                        color: Color.fromRGBO(134, 71, 173, 1),
-                      ),
-                    ),
-                    prefixIcon: Icon(
-                      Icons.email_outlined,
-                      color: Color.fromRGBO(134, 71, 173, 1),
-                    ),
-                    hintText: 'E-mail',
-                    hintStyle: TextStyle(
-                      color: Colors.white,
-                    ),
+                const SizedBox(
+                  height: 35,
+                ),
+                const Text(
+                  'Já tem cadastro?',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 28,
                   ),
                 ),
-              ),
-              Container(
-                width: double.infinity,
-                height: 50,
-                margin: const EdgeInsets.all(15),
-                alignment: Alignment.centerLeft,
-                child: const TextField(
-                  decoration: InputDecoration(
+                const SizedBox(
+                  height: 10,
+                ),
+                const Text(
+                  'Faça seu login e make the change._',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 15,
+                  ),
+                ),
+                const SizedBox(
+                  height: 35,
+                ),
+                Container(
+                  width: double.infinity,
+                  height: 50,
+                  margin: const EdgeInsets.all(15),
+                  alignment: Alignment.centerLeft,
+                  child: const TextField(
+                    style: TextStyle(color: Colors.white),
+                    decoration: InputDecoration(
                       enabledBorder: UnderlineInputBorder(
                         borderSide: BorderSide(
                           color: Color.fromRGBO(134, 71, 173, 1),
                         ),
                       ),
                       prefixIcon: Icon(
-                        Icons.lock_outline,
+                        Icons.email_outlined,
                         color: Color.fromRGBO(134, 71, 173, 1),
                       ),
-                      hintText: 'Senha',
+                      hintText: 'E-mail',
                       hintStyle: TextStyle(
                         color: Colors.white,
-                      ),
-                      suffixIcon: Icon(
-                        Icons.visibility_outlined,
-                        color: Color.fromRGBO(134, 71, 173, 1),
-                      )),
-                ),
-              ),
-              const SizedBox(
-                height: 35,
-              ),
-              Container(
-                width: double.infinity,
-                height: 50,
-                margin: const EdgeInsets.all(15),
-                alignment: Alignment.center,
-                child: SizedBox(
-                  width: double.infinity,
-                  child: TextButton(
-                    onPressed: () => {},
-                    style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all(
-                        const Color.fromRGBO(134, 71, 173, 1),
-                      ),
-                      shape: MaterialStateProperty.all(
-                        RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                      ),
-                      fixedSize: MaterialStateProperty.all(
-                        const Size.fromHeight(45),
-                      ),
-                    ),
-                    child: const Text(
-                      'ENTRAR',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 16,
                       ),
                     ),
                   ),
                 ),
-              ),
-              const SizedBox(
-                height: 100,
-              ),
-              const Text(
-                'Esqueci minha senha',
-                style: TextStyle(
-                  color: Colors.yellow,
-                  fontSize: 16,
+                Container(
+                  width: double.infinity,
+                  height: 50,
+                  margin: const EdgeInsets.all(15),
+                  alignment: Alignment.centerLeft,
+                  child: const TextField(
+                    style: TextStyle(color: Colors.white),
+                    decoration: InputDecoration(
+                        enabledBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Color.fromRGBO(134, 71, 173, 1),
+                          ),
+                        ),
+                        prefixIcon: Icon(
+                          Icons.lock_outline,
+                          color: Color.fromRGBO(134, 71, 173, 1),
+                        ),
+                        hintText: 'Senha',
+                        hintStyle: TextStyle(
+                          color: Colors.white,
+                        ),
+                        suffixIcon: Icon(
+                          Icons.visibility_outlined,
+                          color: Color.fromRGBO(134, 71, 173, 1),
+                        )),
+                  ),
                 ),
-              ),
-              const SizedBox(
-                height: 15,
-              ),
-              const Text(
-                'Criar conta',
-                style: TextStyle(
-                  color: Colors.green,
-                  fontSize: 16,
+                const SizedBox(
+                  height: 35,
                 ),
-              ),
-            ],
+                Container(
+                  width: double.infinity,
+                  height: 50,
+                  margin: const EdgeInsets.all(15),
+                  alignment: Alignment.center,
+                  child: SizedBox(
+                    width: double.infinity,
+                    child: TextButton(
+                      onPressed: () => {},
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all(
+                          const Color.fromRGBO(134, 71, 173, 1),
+                        ),
+                        shape: MaterialStateProperty.all(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                        fixedSize: MaterialStateProperty.all(
+                          const Size.fromHeight(45),
+                        ),
+                      ),
+                      child: const Text(
+                        'ENTRAR',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 100,
+                ),
+                const Text(
+                  'Esqueci minha senha',
+                  style: TextStyle(
+                    color: Colors.yellow,
+                    fontSize: 16,
+                  ),
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                const Text(
+                  'Criar conta',
+                  style: TextStyle(
+                    color: Colors.green,
+                    fontSize: 16,
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
