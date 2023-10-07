d24f858cd1f4905390c8e9bc9e30a5f943dd7051
Lucas Magnago de Oliveira <lucas.magnago.02@gmail.com>
2023-10-07 10:49:34 -0300

    utilizando listTile, wrap e showModalBottomSheet para exibir informações ao usuário

--- lib/shared/widgets/custom_drawer.dart
+++ lib/shared/widgets/custom_drawer.dart
@@ -10,55 +10,102 @@ class CustomDrawer extends StatelessWidget {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          UserAccountsDrawerHeader(
-            accountName: const Text('Lucas'),
-            accountEmail: const Text('lucas@gmail.com'),
-            currentAccountPicture: CircleAvatar(
-              child: Image.asset(
-                './assets/icons/user.png',
-                scale: 0.8,
+          InkWell(
+            onTap: () {
+              showModalBottomSheet(
+                  context: context,
+                  builder: (BuildContext bc) {
+                    return Wrap(
+                      children: [
+                        ListTile(
+                          onTap: () {
+                            Navigator.pop(context);
+                          },
+                          leading: const Icon(Icons.camera),
+                          title: const Text('Camera'),
+                        ),
+                        ListTile(
+                          onTap: () {
+                            Navigator.pop(context);
+                          },
+                          leading: const Icon(Icons.photo_album),
+                          title: const Text('Galeria'),
+                        ),
+                      ],
+                    );
+                  });
+            },
+            child: UserAccountsDrawerHeader(
+              accountName: const Text('Lucas'),
+              accountEmail: const Text('lucas@gmail.com'),
+              currentAccountPicture: CircleAvatar(
+                child: Image.asset(
+                  './assets/icons/user.png',
+                  scale: 0.8,
+                ),
               ),
             ),
           ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              InkWell(
-                onTap: () => {
-                  Navigator.pop(context),
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => const DadosCadastraisPage(),
-                    ),
-                  ),
-                },
-                child: Container(
-                  padding:
-                      const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
-                  width: double.infinity,
-                  child: const Text('Dados cadastrais'),
+          InkWell(
+            onTap: () => {
+              Navigator.pop(context),
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const DadosCadastraisPage(),
                 ),
               ),
-              const Divider(),
-              InkWell(
-                onTap: () => {},
-                child: Container(
-                  padding:
-                      const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
-                  child: const Text('Termos de uso e privacidade'),
-                ),
-              ),
-              const Divider(),
-              InkWell(
-                  onTap: () => {},
-                  child: Container(
-                      padding: const EdgeInsets.symmetric(
-                          vertical: 5, horizontal: 10),
-                      child: const Text('Configurações'))),
-              const Divider(),
-            ],
+            },
+            child: Container(
+              padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+              width: double.infinity,
+              child: const Text('Dados cadastrais'),
+            ),
           ),
+          const Divider(),
+          InkWell(
+            onTap: () => {
+              showModalBottomSheet(
+                context: context,
+                builder: (BuildContext bc) {
+                  return Container(
+                    padding: const EdgeInsets.all(16),
+                    child: const Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Text(
+                          'Termos de uso e privacidade',
+                          style: TextStyle(
+                            fontSize: 23,
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Text(
+                          "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+                          textAlign: TextAlign.justify,
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              )
+            },
+            child: Container(
+              padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+              child: const Text('Termos de uso e privacidade'),
+            ),
+          ),
+          const Divider(),
+          InkWell(
+              onTap: () => {},
+              child: Container(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                  child: const Text('Configurações'))),
+          const Divider(),
         ],
       ),
     );
